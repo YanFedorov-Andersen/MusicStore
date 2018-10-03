5d3d189f5f3de85253199d27488930959c0dce37
yan <axxxx@ukr.net>
2018-10-03 11:16:37 +0300

    add repository

---
+++ MusicStore.DataAccess/Interfaces/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace MusicStore.DataAccess.Interfaces
+{
+    public interface IRepository<T>
+    {
+        IEnumerable<T> GetItemList();
+        T GetItem(int id);
+        int Create(T item);
+        int Update(T item);
+        int Delete(int id);
+    }
+}
---
+++ MusicStore.DataAccess/Realization/UserAccountRepository.cs
@@ -0,0 +1,63 @@
+﻿using MusicStore.DataAccess.Interfaces;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+
+namespace MusicStore.DataAccess.Realization
+{
+    public class UserAccountRepository : IRepository<User>
+    {
+        private readonly MusicStoreContext _dataBase;
+
+        public UserAccountRepository(MusicStoreContext dataBase)
+        {
+            _dataBase = dataBase;
+        }
+        public int Create(User item)
+        {
+            if (item != null)
+            {
+                _dataBase.Users.Add(item);
+                _dataBase.SaveChanges();
+                return item.Id;
+            }
+            return -1;
+        }
+
+        public int Delete(int id)
+        {
+            User user = _dataBase.Users.Find(id);
+
+            if (user == null)
+            {
+                return -1;
+            }
+
+            _dataBase.Users.Remove(user);
+            _dataBase.SaveChanges();
+            return id;
+
+        }
+
+        public User GetItem(int id)
+        {
+            return _dataBase.Users.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IEnumerable<User> GetItemList()
+        {
+            return _dataBase.Users.ToList();
+        }
+
+        public int Update(User item)
+        {
+            if (item != null)
+            {
+                _dataBase.Entry(item).State = EntityState.Modified;
+                _dataBase.SaveChanges();
+                return item.Id;
+            }
+            return -1;
+        }
+    }
+}
